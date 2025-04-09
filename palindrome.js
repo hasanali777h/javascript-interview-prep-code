eed770e39c60c61ce1d2d983ef9afbbec319e0d0
hasanali777h <hasanali777h@gmail.com>
2025-04-09 11:52:21 +0500

    new added

--- palindrome.js
+++ palindrome.js
@@ -4,5 +4,14 @@
 }*/
 
 //call function
-const output = isPalindrome('racecar');
-console.log(output);
+// const output = isPalindrome("racecar");
+// console.log(output);
+
+const isPalindrome = (str) => {
+  let reverse = "";
+  for (let char of str) {
+    reverse = char + reverse;
+  }
+  return reverse == str ? "palindrome" : "not palindrome";
+};
+console.log(isPalindrome("madam"));
