e55918fbc9c9e004a451737d53e6df343bef4357
hasanali777h <hasanali777h@gmail.com>
2026-07-01 21:48:10 +0500

    date funtions and methods added

---
+++ dates.js
@@ -0,0 +1,54 @@
+const date = new Date();
+console.log(date, "date");
+/*
+Date string methods
+*/
+// console.log(date.valueOf(1))
+// console.log(date.toUTCString())
+// console.log(date.toTimeString())
+// console.log(date.toString())
+// console.log(date.toLocaleTimeString())
+// console.log(date.toLocaleString())
+// console.log(date.toLocaleDateString())
+// console.log(date.toJSON())
+// console.log(date.toISOString())
+// console.log(date.toDateString())
+/*
+Date setter methods
+*/
+// console.log(date.setUTCSeconds(55))
+// console.log(date.setUTCMonth(5))
+// console.log(date.setUTCMinutes(6))
+// console.log(date.setUTCMilliseconds(2))
+// console.log(date.setUTCHours(3))
+// console.log(date.setUTCFullYear(1))
+// console.log(date.setUTCDate(9))
+// console.log(date.setTime(5))
+// console.log(date.setSeconds(4))
+// console.log(date.setMonth(7))
+// console.log(date.setMinutes(3))
+// console.log(date.setMilliseconds(1))
+// console.log(date.setHours(9))
+// console.log(date.setFullYear(8))
+// console.log(date.setDate(2))
+/*
+Date getter methods
+*/
+// console.log(date.getVarDate()) // obselete methoh used for IE-6
+// console.log(date.getUTCSeconds())
+// console.log(date.getUTCMonth())
+// console.log(date.getUTCMilliseconds())
+// console.log(date.getUTCHours())
+// console.log(date.getUTCFullYear())
+// console.log(date.getUTCDay())
+// console.log(date.getUTCDate())
+// console.log(date.getTimezoneOffset())
+// console.log(date.getTime())
+// console.log(date.getSeconds())
+// console.log(date.getMonth())
+// console.log(date.getMinutes())
+// console.log(date.getMilliseconds())
+// console.log(date.getHours())
+// console.log(date.getFullYear())
+// console.log(date.getDay())
+// console.log(date.getDate())
